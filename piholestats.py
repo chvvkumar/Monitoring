94ebe874da632c5d4365131bc1e6da3412a233ba
Kumar Challa <chvvkumar@users.noreply.github.com>
2018-03-25 11:24:49 -0500

    Add files via upload

---
+++ piholestats.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import json
+import requests
+
+# Note: Change localhost to the correct IP if needed.
+r = requests.get('http://pi.hole/admin/api.php')
+rstats = json.loads(r.text)
+
+#stats = {}
+#for x in rstats:
+#    stats[x] = float(rstats[x].replace(',', ''))
+
+print json.dumps(rstats)
